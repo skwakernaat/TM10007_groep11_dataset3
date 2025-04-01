44efcfd2227c6e00b2d7a6ea2ea1e4f252cf122e
Stijn Kwakernaat <stijnjawi@gmail.com>
2025-04-01 16:11:44 +0200

    added main

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import seaborn
+import pandas as pd
+import torch
+import itertools
+import matplotlib.pyplot as plt
+import statistics
+import math
+from worcgist.load_data import load_data
+from sklearn import model_selection
+from sklearn import model_selection
+from sklearn import metrics
+from sklearn import feature_selection
+from sklearn import preprocessing
+from sklearn import neighbors
+from sklearn import svm
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
