1c7232f07aeb329c42fad69e5b5e02a44535edc2
Stijn Kwakernaat <stijnjawi@gmail.com>
2025-04-08 12:51:39 +0200

    changed the output of linear_classifier

--- linear_classifiers.py
+++ linear_classifiers.py
@@ -5,8 +5,9 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
+from evaluate_model_with_kfold import evaluate_model_with_kfold
 
-def linear_classifier(data_train, data_val, labels_train, labels_val):
+def linear_classifier(data_train, labels_train, n_folds=5):
     '''This function trains a linear classifier on the data and evaluates its performance.'''
     # Define classifiers
     classifiers = {
@@ -14,12 +15,13 @@ def linear_classifier(data_train, data_val, labels_train, labels_val):
         "LDA": LinearDiscriminantAnalysis(),
         "Linear SVM": SVC(kernel='linear'),
         "SGD Classifier": SGDClassifier(loss='log_loss', max_iter=1000)
-}
+    }
 
+    results_list = []
+
+    # Train and evaluate each classifier
     for name, clf in classifiers.items():
-        clf.fit(data_train, labels_train)
-        y_pred = clf.predict(data_val)
-        acc = accuracy_score(labels_val, y_pred)
-        print(f"\n{name}")
-        print("Accuracy:", round(acc, 3))
-        print(classification_report(labels_val, y_pred))
+        result = evaluate_model_with_kfold(data_train, labels_train, clf, n_folds)
+        results_list.append(result)
+
+    return results_list
