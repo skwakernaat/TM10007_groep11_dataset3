3a36d9a5660924f3e9d80c4d6646a0f8a8aa5727
Rosalie van der Weerdt <revanderweerdt@gmail.com>
2025-04-16 11:39:36 +0200

    altered datasplitting, now uses sklearn method and is not random

--- split_data.py
+++ split_data.py
@@ -1,21 +1,23 @@
-'''This module contains the function split_data which is used to split the data into a training, validation and test set.'''
+'''This module contains the function split_data which is used to split the data into a training and test set.'''
 
-from sklearn import model_selection
+from sklearn.model_selection import train_test_split
 
-def split_data(data):
-    '''This function splits the data into a training, validation and test set.'''
-    # Extract label column from data
-    labels = data['label']
-    data_nolabels = data.drop(columns=['label'])
+def split_and_save_gist_dataset(df):
+    """
+    Splits the GIST dataset into train and test sets with stratification and returns the train and test DataFrames.
 
-    # Convert to array
-    labels_array = labels.to_numpy()
-    data_array = data_nolabels.to_numpy()
+    Parameters:
+    - df: DataFrame of the GIST dataset.
 
-    # Convert to integer
-    labels_array = labels_array.astype(int)
+    Returns:
+    - (train_df, test_df): Tuple of train and test DataFrames.
+    """
 
-    # Splitting data into train and test data sets. 80% is used for training set. Ratio of GIST and non-GIST is kept the same (50/50)
-    data_train, data_test, labels_train, labels_test = model_selection.train_test_split(data_array, labels_array, test_size=0.20, stratify=labels_array)
-
-    return data_train, data_test, labels_train, labels_test
+    # Stratified train-test split on the "label" column
+    train_df, test_df = train_test_split(
+        df,
+        test_size=0.2,
+        stratify=df["label"],
+        random_state=42
+    )
+    return train_df, test_df
