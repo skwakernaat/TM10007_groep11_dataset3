09c2f3497c16c30f34817c27585d6fd7b3d4be54
Stijn Kwakernaat <stijnjawi@gmail.com>
2025-04-17 16:04:20 +0200

    fixed coutning in balance_check again

--- preprocessing/check_data_balance.py
+++ preprocessing/check_data_balance.py
@@ -1,13 +1,16 @@
 '''This module contains the function balance_data which is used to balance the data by checking if the amount of 1's and 0's are (approximately) equal (check for data imbalance).'''
 
+import numpy as np
+
 def check_balance(y_train):
     '''This function checks if the amount of 1's and 0's are (approximately) equal (check for data imbalance).'''
     y_train_balanced = y_train.copy()
 
     # Check if the amount of 1's and 0's are (approximately) equal
     # (check for data imbalance)
-    count_gist = y_train_balanced.count(1)
-    count_non_gist = y_train_balanced.count(0)
+    counts = dict(zip(*np.unique(y_train_balanced, return_counts=True)))
+    count_gist = counts.get(1, 0)
+    count_non_gist = counts.get(0, 0)
 
     percentage_gist = count_gist / (count_gist+count_non_gist) * 100
     percentage_non_gist = count_non_gist / (count_gist+count_non_gist) * 100
