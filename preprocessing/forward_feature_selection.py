bugfix of wrong formula for amount of features. Added space in balance message

preprocessing/forward_feature_selection.py
@@ -2,6 +2,7 @@
     features using sklearn's SequentialFeatureSelector. It takes the training data and the number of
     features to select as input and returns the reduced dataset with the label.'''
 
+import math
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SequentialFeatureSelector
@@ -12,7 +13,10 @@ def forward_feature_selection(X_train, y_train, X_test, y_test, feature_names):
     # Calculate the number of features to select
     n_samples = len(y_train) + len(y_test)
     n_classes = len(np.unique(y_train))
-    n_features = n_samples / (2 * n_classes)
+    n_features = math.floor(n_samples / (10 * n_classes))
+    print(n_samples)
+    print(n_classes)
+    print(n_features)
 
     # Create model and feature selector
     model = RandomForestClassifier(n_estimators=100, random_state=42)